1d7167f4380a9228d82b5d966c5a83bac4204b97
Robert Orlikowski <38508936+robert72127@users.noreply.github.com>
2026-05-24 11:19:38 +0200

    Simplify internal state of radix cache (#5)
    * move tree class outside
    
    * reshuffle
    
    * much simpler ,almost there
    
    * ok finish new design

--- kvcache.py
+++ kvcache.py
@@ -1,5 +1,4 @@
 from collections import deque
-from collections import OrderedDict
 import torch
 
 def blocks_for_tokens(token_count: int, block_size: int):
@@ -7,6 +6,103 @@ def blocks_for_tokens(token_count: int, block_size: int):
     if token_count > 0 and token_count % block_size == 0:
         blocks_cnt += 1
     return blocks_cnt
+
+class RadixTree:
+    def __init__(self, kv_cache):
+        self.root = self.RadixNode(page_id=None, key=None)
+        self.uuid_to_last_node = {}
+
+    def get_last_node(self, uuid):
+        return self.uuid_to_last_node[uuid]
+
+    def add_node(self, uuid, node):
+        self.uuid_to_last_node[uuid] = node
+
+    def remove_uuid(self, uuid):
+        self.uuid_to_last_node.pop(uuid, None)
+
+    def remove_node(self, node):
+        if node == self.root:
+            return
+        for uuid, last_node in list(self.uuid_to_last_node.items()):
+            current = last_node
+            while current is not None:
+                if current is node:
+                    self.uuid_to_last_node[uuid] = self.root
+                    break
+                current = current.parent
+        for child in list(node.children):
+            self.remove_node(child)
+        if node.parent:
+            node.parent.children = [child for child in node.parent.children if child is not node]
+
+    def insert(self, uuid, block_idx, key):
+        node = self.get_last_node(uuid)
+        for child in node.children:
+            if child.key == key:
+                self.uuid_to_last_node[uuid] = child
+                return child
+        new_node = self.RadixNode(page_id=block_idx, key=key)
+        new_node.parent = node
+        node.children.append(new_node)
+        self.uuid_to_last_node[uuid] = new_node
+        return new_node
+        
+    def search(self, toks):
+        return self.root.search(toks, [])
+    
+    class RadixNode:
+        def __init__(self, page_id, key):
+            self.key=key
+            self.page_id = page_id
+            self.parent = None
+            self.children = []
+
+        def search(self, key_lst, nodes_list):
+            if not key_lst:
+                return nodes_list
+            for child in self.children:
+                if child.key == key_lst[0]:
+                    return child.search(key_lst[1:], nodes_list + [child])
+            return nodes_list
+
+class BlockAllocator:
+    def __init__(self, num_blocks):
+        self.locked_slots = {}
+        
+        self.evictable_blocks = deque()
+        
+        self.free_slots = deque([i for i in range (num_blocks)])
+        self.free_slots_cnt = num_blocks
+
+    def incr_lock_count(self, blocks):
+        for block in blocks:
+            if block in self.evictable_blocks:
+                self.evictable_blocks.remove(block)
+                self.locked_slots[block] = 1
+            else:
+                self.locked_slots[block] = self.locked_slots.get(block, 0) + 1
+
+    def free_blocks(self, blocks):
+        # by traversing in revese order lower nodes from radix will be evicted first
+        for block in reversed(blocks):
+            if block is None or block not in self.locked_slots:
+                continue
+            self.locked_slots[block] -= 1
+            if self.locked_slots[block] == 0:
+                del self.locked_slots[block]
+                self.evictable_blocks.append(block)
+
+    def get_free_blocks(self, block_cnt=1):
+        blocks = []
+        while len(blocks) < block_cnt and self.free_slots:
+            block = self.free_slots.popleft()
+            self.free_slots_cnt -= 1
+            blocks.append(block)
+        while len(blocks) < block_cnt:
+            blocks.append(self.evictable_blocks.popleft())
+        return blocks
+
 
 class RadixKVCache:
     class UUIDState:
@@ -46,7 +142,7 @@ class RadixKVCache:
             if self.write_block_occupancy == self.kv_cache.block_size:
                self.write_block = self.kv_cache.radix_insert(self.uuid, self.write_block, self.write_block_toks)
                self.commited_blocks += [self.write_block]
-               self.write_block = self.kv_cache.get_free_blocks()[0]
+               self.write_block = self.kv_cache.allocate_blocks()[0]
                self.write_block_occupancy = 0 
                self.blocks_count +=1
                self.write_block_toks = []
@@ -56,60 +152,6 @@ class RadixKVCache:
             self.write_block_toks.append(int(tok.item()) if torch.is_tensor(tok) else int(tok))
             self.write_block_occupancy+=1
 
-    class RadixNode:
-        def __init__(self, page_id, key, kv_cache):
-            self.key=key
-            self.page_id = page_id
-            self.kv_cache = kv_cache
-            self.parent = None
-            self.children = []
-
-        def search(self, key_lst, nodes_list):
-            if not key_lst:
-                return nodes_list
-            for child in self.children:
-                if child.page_id not in self.kv_cache.resident_blocks:
-                    continue
-                if child.key == key_lst[0]:
-                    self.kv_cache.locked_slots[child.page_id] = self.kv_cache.locked_slots.get(child.page_id, 0) + 1
-                    self.kv_cache._touch_resident_block(child.page_id)
-                    return child.search(key_lst[1:], nodes_list + [child])
-            return nodes_list
-
-    class RadixTree:
-        def __init__(self, kv_cache):
-            self.root = RadixKVCache.RadixNode(page_id=None, key=None, kv_cache=kv_cache)
-            self.uuid_to_last_node = {}
-            self.kv_cache = kv_cache
-
-        def get_last_node(self, uuid):
-            return self.uuid_to_last_node[uuid]
-
-        def add_node(self, uuid, node):
-            self.uuid_to_last_node[uuid] = node
-            
-        def insert(self, uuid, block_idx, key):
-            node = self.get_last_node(uuid)
-            for child in node.children:
-                if child.page_id in self.kv_cache.resident_blocks and child.key == key:
-                    self.kv_cache.locked_slots[child.page_id] = self.kv_cache.locked_slots.get(child.page_id, 0) + 1
-                    self.kv_cache._touch_resident_block(child.page_id)
-                    self.uuid_to_last_node[uuid] = child
-                    self.kv_cache.free_slots.append(block_idx)
-                    self.kv_cache.free_slots_cnt += 1
-                    del self.kv_cache.locked_slots[block_idx]
-                    return child.page_id
-            new_node = RadixKVCache.RadixNode(page_id=block_idx, key=key, kv_cache=self.kv_cache)
-            new_node.parent = node
-            node.children.append(new_node)
-            self.kv_cache.block_to_node[block_idx] = new_node
-            self.kv_cache._register_resident_block(block_idx)
-            self.uuid_to_last_node[uuid] = new_node
-            return block_idx
-
-        def search(self, toks):
-            return self.root.search(toks, [])
-        
     def __init__(self, d_head, head_cnt, num_blocks, block_size, max_requests_per_uuid=None, device=None, dtype=None):
         self.d_head = d_head
         self.head_cnt = head_cnt
@@ -120,70 +162,76 @@ class RadixKVCache:
         self.uuids = {}
         
         # blocks are put there when they are used by at least one active uuid
-        self.locked_slots = {}
-        self.resident_blocks = set()
-        self.resident_lru = OrderedDict()
         self.block_to_node = {}
 
-        self.free_slots = deque([i for i in range (num_blocks)])
-        self.free_slots_cnt = num_blocks
+        self.block_allocator = BlockAllocator(num_blocks)
 
         self.device = device
         self.dtype = dtype
         self.K = torch.zeros((num_blocks,self.head_cnt, block_size, self.d_head), device=device, dtype=dtype)
         self.V = torch.zeros((num_blocks,self.head_cnt, block_size, self.d_head), device=device, dtype=dtype)
 
-        self.radix_tree = self.RadixTree(self)
+        self.radix_tree = RadixTree(self)
 
     # might return different index if block is already present, then this block will be auto freed, and found block lock_count will be incremented
     def radix_insert(self, uuid, block_idx, toks):
-        return self.radix_tree.insert(uuid, block_idx, toks) 
+        new_node = self.radix_tree.insert(uuid, block_idx, toks) 
+        new_block_idx = new_node.page_id
+        self.block_to_node[new_block_idx] = new_node
+        if new_block_idx != block_idx:
+            self.block_allocator.free_blocks([block_idx])
+        return new_block_idx
 
     # returns longest list of pages that match prefix, update locked_slots if found pages
     def radix_search(self, uuid, toks):
         nodes = self.radix_tree.search(toks)
+        pages = [node.page_id for node in nodes]
+        self.block_allocator.incr_lock_count(pages)
         self.radix_tree.add_node(uuid, nodes[-1] if nodes else self.radix_tree.root)
-        return [node.page_id for node in nodes], len(nodes)
+        return pages, len(nodes)
 
-    def get_blocks_available(self):
-        reserved=0
-        for uuid in self.uuids:
-            uuid_state = self.uuids[uuid]
-            reserved += self.max_requests_per_uuid - uuid_state.blocks_count
-        
-        return self.free_slots_cnt + len(self.resident_lru) - reserved
+    def _release(self, uuid):
+        uuid_state = self.uuids.pop(uuid, None)
+        if uuid_state is None:
+            self.radix_tree.remove_uuid(uuid)
+            return
+        self.radix_tree.remove_uuid(uuid)
 
-    def get_free_blocks(self, block_cnt=1):
-        blocks = []
-        for _ in range(block_cnt):
-            if self.free_slots:
-                block = self.free_slots.popleft()
-                self.free_slots_cnt -= 1
-            else:
-                block = self._evict_one_resident_block()
-            blocks.append(block)
-        self.locked_slots.update({block: 1 for block in blocks})
+        blocks = uuid_state.commited_blocks + [uuid_state.write_block]
+        self.block_allocator.free_blocks(blocks)
+
+    def release(self, uuids):
+        for uuid in uuids:
+            self._release(uuid)
+
+    def allocate_blocks(self, block_cnt=1):
+        blocks = self.block_allocator.get_free_blocks(block_cnt)
+        # if they were in tree remove from tree
+        for block in blocks:
+            if block in self.block_to_node:
+                node = self.block_to_node.pop(block)
+                self._drop_subtree_refs([node])
+                self.radix_tree.remove_node(node)
+        self.block_allocator.incr_lock_count(blocks)
         return blocks
 
-    def _free_blocks(self, uuid):
-        if uuid in self.uuids:
-            uuid_state =self.uuids[uuid]
-            for block in uuid_state.commited_blocks:
-                self.locked_slots[block] -= 1
-                if self.locked_slots[block] == 0:
-                    del self.locked_slots[block]
-                    self._touch_resident_block(block)
-            self.locked_slots[uuid_state.write_block] -= 1
-            if self.locked_slots[uuid_state.write_block] == 0:
-                del self.locked_slots[uuid_state.write_block]
-                self.free_slots.append(uuid_state.write_block)
-                self.free_slots_cnt += 1
-            self.uuids.pop(uuid)
+    def _drop_subtree_refs(self, nodes):
+        stack = list(nodes)
+        while stack:
+            node = stack.pop()
+            stack.extend(node.children)
+            self.block_to_node.pop(node.page_id, None)
 
     def get_pages(self, indexes):
         K_ = [self.K[ind] for ind in indexes]
         V_ = [self.V[ind] for ind in indexes]
         return K_, V_
+
+    def get_blocks_available(self):
+        reserved = 0
+        for uuid_state in self.uuids.values():
+            reserved += self.max_requests_per_uuid - uuid_state.blocks_count
+        return self.block_allocator.free_slots_cnt + len(self.block_allocator.evictable_blocks) - reserved
 
     def _block_keys_for_tokens(self, toks):
         if torch.is_tensor(toks):
@@ -192,27 +240,6 @@ class RadixKVCache:
             toks[i:i + self.block_size]
             for i in range(0, len(toks) - len(toks) % self.block_size, self.block_size)
         ]
-
-    def _register_resident_block(self, block):
-        self.resident_blocks.add(block)
-        self.resident_lru.pop(block, None)
-
-    def _touch_resident_block(self, block):
-        if block not in self.resident_blocks:
-            return
-        if block in self.locked_slots:
-            self.resident_lru.pop(block, None)
-            return
-        self.resident_lru.pop(block, None)
-        self.resident_lru[block] = None
-
-    def _evict_one_resident_block(self):
-        block, _ = self.resident_lru.popitem(last=False)
-        self.resident_blocks.remove(block)
-        node = self.block_to_node.pop(block, None)
-        if node is not None and node.parent is not None:
-            node.parent.children = [child for child in node.parent.children if child is not node]
-        return block
 
     def get_q_position(self, uuids):
         positions = []
@@ -249,7 +276,7 @@ class RadixKVCache:
             if blocks_cnt == 0:
                 blocks_cnt = 1
 
-            indexes = self.get_free_blocks(blocks_cnt)
+            indexes = self.allocate_blocks(blocks_cnt)
             pages_K, pages_V = self.get_pages(indexes)
 
             for j in range(remaining_tokens):
@@ -295,7 +322,3 @@ class RadixKVCache:
             batch_size += 1
             pages_info += [uuid_state.get_pages_info()]
         return pages_info
-
-    def release(self, uuids):
-        for uuid in uuids:
-            self._free_blocks(uuid)
